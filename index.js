3fb64b2f322082158ec52ab06c71cc47165f3d25
Miyuki Abe <miyuki0809@gmail.com>
2018-04-09 00:19:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,9 +3,6 @@ function happyHolidays(string) {
   return `Happy holidays!`
 }
 
-function happyHolidaysTo(neme) {
-  return `Happy holidays, ${name}!`
-}
 
 function happyHolidayTo(holiday, name) {
   return `Happy ${holiday}, ${name}!`
